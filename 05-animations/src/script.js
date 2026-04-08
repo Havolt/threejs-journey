5480a4feca6498d2851f023276e29281debb2a63
Mark <markfitz815@gmail.com>
2026-04-08 21:45:38 +0100

    Add 06 starter files and move gsap to 05 folder

--- 05-animations/src/script.js
+++ 05-animations/src/script.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import gsap from "gsap";
+
+console.log(gsap);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -8,7 +11,7 @@ const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 0x6655bb });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -30,20 +33,31 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
+// Clock
+const clock = new THREE.Clock();
+
 // Time
-let time = Date.now();
+// let time = Date.now();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
 
 // Animations
 const tick = () => {
   //   console.log("tick");
 
-  // Time
-  const currentTime = Date.now();
-  const deltaTime = currentTime - time;
-  time = currentTime;
-  console.log(deltaTime);
+  const elapsedTime = clock.getElapsedTime();
+  //   console.log(elapsedTime);
 
-  mesh.rotation.y += 0.002 * deltaTime;
+  // Time
+  //   const currentTime = Date.now();
+  //   const deltaTime = currentTime - time;
+  //   time = currentTime;
+  //   console.log(deltaTime);
+
+  //   mesh.rotation.y = Math.PI * 2 * elapsedTime;
+  //   mesh.position.y = Math.sin(elapsedTime);
+  //   mesh.position.x = Math.cos(elapsedTime);
 
   // Update objects
 
