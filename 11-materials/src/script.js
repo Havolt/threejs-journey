729606973c08c7362752e291148d9fe7c9849230
Mark <markfitz815@gmail.com>
2026-04-13 21:53:53 +0100

    Add materials and lights for part 11

---
+++ 11-materials/src/script.js
@@ -0,0 +1,166 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+// Environment map texture
+const matcapTexture = textureLoader.load("textures/matcaps/1.png");
+
+// Door textures
+const alphaTexture = textureLoader.load("textures/door/alpha.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "textures/door/ambientOcclusion.jpg",
+);
+const doorColorTexture = textureLoader.load("textures/door/color.jpg");
+const doorHeightTexture = textureLoader.load("textures/door/height.jpg");
+const doorMetalnessTexture = textureLoader.load("textures/door/metalness.jpg");
+const doorNormalTexture = textureLoader.load("textures/door/normal.jpg");
+const doorRoughnessTexture = textureLoader.load("textures/door/roughness.jpg");
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace;
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
+
+// Gradient textures
+const gradientTexture3 = textureLoader.load("textures/gradients/3.jpg");
+const gradientTexture5 = textureLoader.load("textures/gradients/5.jpg");
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+// const material = new THREE.MeshBasicMaterial({
+//   // color: 0x44ebeb,
+//   map: doorColorTexture,
+// });
+
+// const material = new THREE.MeshBasicMaterial();
+// material.map = doorColorTexture;
+// material.color = new THREE.Color(0x22baba);
+// material.wireframe = true;
+// material.transparent = true;
+// material.opacity = 0.5;
+// material.alphaMap = alphaTexture;
+// material.side = THREE.DoubleSide;
+
+// const material = new THREE.MeshNormalMaterial();
+
+// matcap material
+// const material = new THREE.MeshMatcapMaterial();
+// material.matcap = matcapTexture;
+
+// MeshDepthMaterial
+// const material = new THREE.MeshDepthMaterial();
+
+// MeshLambertMaterial
+const material = new THREE.MeshLambertMaterial();
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+sphere.position.x = -1.5;
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+plane.position.x = 0;
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  material,
+);
+torus.position.x = 1.5;
+
+scene.add(sphere, plane, torus);
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// Point light
+const pointLight = new THREE.PointLight(0xffffff, 30);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  sphere.rotation.x = 0.15 * elapsedTime;
+  sphere.rotation.y = 0.1 * elapsedTime;
+
+  plane.rotation.x = 0.15 * elapsedTime;
+  plane.rotation.y = 0.1 * elapsedTime;
+
+  torus.rotation.x = 0.15 * elapsedTime;
+  torus.rotation.y = 0.1 * elapsedTime;
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
